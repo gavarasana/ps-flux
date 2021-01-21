4a5ff18c7134ed1f195d94fe9045508e2767de3f
gavarasana <gavarasana@gmail.com>
2021-01-21 07:04:54 -0500

    added validation for course form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -3,6 +3,7 @@ import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = (props) => {
+
   const [course, setCourse] = useState({
     id: null,
     title: "",
@@ -10,19 +11,35 @@ const ManageCoursePage = (props) => {
     category: "",
   });
 
+  const [errors, setErrors] = useState({});
+
+  function isFormValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required.";
+    if (!course.authorId) _errors.authorId = "Author is required.";
+    if (!course.category) _errors.category = "Category is required.";
+
+    setErrors(_errors);
+
+    return (Object.keys(errors).length === 0);
+  }
+
+
   function handleChange({ target }) {
     setCourse({ ...course, [target.name]: target.value });
   }
 
   function onSubmit(event) {
     event.preventDefault();
+    if (!isFormValid()) return;
     courseApi.saveCourse(course);
   }
 
   return (
     <>
       <h1>Manage Course</h1>
-      <CourseForm course={course} onChange={handleChange} onSubmit={onSubmit} />
+      <CourseForm course={course} errors={errors} onChange={handleChange} onSubmit={onSubmit} />
     </>
   );
 };
