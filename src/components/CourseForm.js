764e6ca226ecb8236ec74a8d5f1443b627bdb977
gavarasana <gavarasana@gmail.com>
2021-01-20 19:50:32 -0500

    Added course page

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,39 +1,45 @@
 import React from "react";
+import TextInput from "./common/TextInput";
 
 function CourseForm(props) {
-    return (
-        <form>
-            <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <div className="field">
-                    <input id="title" name="title"
-                        type="text" className="form-control" value={props.course.title} onChange={props.onChange} />
-                </div>
-            </div>
+  return (
+    <form onSubmit={props.onSubmit}>
+      <TextInput
+        id="title"
+        name="title"
+        label="Title"
+        value={props.course.title}
+        onChange={props.onChange}
+      />
 
-            <div className="form-group">
-                <label htmlFor="author">Author</label>
-                <div className="field">
-                    <select id="author" name="authorId" value={props.course.authorId || ""} onChange={props.onChange} className="form-control">
-                        <option value="" />
-                        <option value="1">Cory House</option>
-                        <option value="2">Scott Allen</option>
-                    </select>
-                </div>
-            </div>
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            value={props.course.authorId || ""}
+            onChange={props.onChange}
+            className="form-control"
+          >
+            <option value="" />
+            <option value="1">Cory House</option>
+            <option value="2">Scott Allen</option>
+          </select>
+        </div>
+      </div>
 
-            <div className="form-group">
-                <label htmlFor="category">Category</label>
-                <div className="field">
-                    <input id="category" name="category"
-                        type="text" onChange={props.onChange} className="form-control" value={props.course.category} />
-                </div>
-            </div>
+      <TextInput
+        id="category"
+        name="category"
+        label="Category"
+        onChange={props.onChange}
+        value={props.course.category}
+      />
 
-            <input type="submit" value="Save" className="btn btn-primary" />
-
-        </form>
-    );
+      <input type="submit" value="Save" className="btn btn-primary" />
+    </form>
+  );
 }
 
 export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
-import { Prompt } from "react-router-dom";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
-const ManageCoursePage = props => {
-    const [course, setCourse] = useState({
-        id: null,
-        title: "",
-        authorId: null,
-        category: ""
+const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    title: "",
+    authorId: null,
+    category: "",
+  });
 
-    });
+  function handleChange({ target }) {
+    setCourse({ ...course, [target.name]: target.value });
+  }
 
-    function handleChange(event) {
-        const updatedCourse = { ...course, [event.target.name]: event.target.value };
-        setCourse(updatedCourse);
-    }
+  function onSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
+  }
 
-    return (
-
-        <>
-            <h1>Manage Course</h1>
-            <CourseForm course={course} onChange={handleChange} />
-        </>
-    );
-}
+  return (
+    <>
+      <h1>Manage Course</h1>
+      <CourseForm course={course} onChange={handleChange} onSubmit={onSubmit} />
+    </>
+  );
+};
 
 export default ManageCoursePage;
